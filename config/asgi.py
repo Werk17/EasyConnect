0e886f1bc5afbcedca2d752c5b140628dcaa1d2f
Charles Werk <charles.werk@umconnect.umt.edu>
2021-12-11 23:38:27 -0700

    heroku fix

--- config/asgi.py
+++ config/asgi.py
@@ -1,26 +1,7 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
-
+import django
+from channels.routing import get_default_application
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import chat.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dropdjango.settings")
+django.setup()
+application = get_default_application()
